ab2840f6b98d30c081b221486fac58d417ba2dfd
Amayuru369 <amayuruperera0@gmail.com>
2026-05-04 14:47:36 +0530

    lab5 answers

---
+++ lab5/lab_5/src/main/java/com/mycompany/lab_5/Lab_5.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_5;
+
+/**
+ *
+ * @author MSI
+ */
+public class Lab_5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+} 
+
