147effac5b7a2569b4aeb1e60b417629bab12f72
Amayuru369 <amayuruperera0@gmail.com>
2026-05-18 14:51:31 +0530

    calculator

---
+++ lab06/src/main/java/com/mycompany/lab06/Lab06.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab06;
+
+/**
+ *
+ * @author MSI
+ */
+public class Lab06 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
